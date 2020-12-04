1c1edf641b240e671313b2cb17e317db776b92ef
Rinat Khusainov <khusainovrm@yandex.ru>
2020-12-04 15:10:20 +0300

    feat: install mongoose for mongoDB

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -14,10 +14,13 @@ import {
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Response, Request } from 'express';
+import { ProductsService } from './products.service';
+// import { Response, Request } from 'express';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
   // @Get()
   // // @Redirect('https://yandex.ru', 301)
   // getAll(@Req() req: Request, @Res() res: Response): string {
@@ -27,20 +30,20 @@ export class ProductsController {
 
   @Get()
   // @Redirect('https://yandex.ru', 301)
-  getAll(): string {
-    return 'get all';
+  getAll(): any[] {
+    return this.productsService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return 'get one id:' + ' ' + id;
+  getOne(@Param('id') id: string) {
+    return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  @Header('Chace-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto): string {
-    return `Title: ${createProductDto.title} Price:${createProductDto.price}`;
+  @Header('Cache-Control', 'none')
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 
   @Delete(':id')
